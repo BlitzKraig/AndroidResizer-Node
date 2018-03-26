23d1ca173f182b403914e5a946c646ebda6576b0
Craig Currie <craig@blitzkraig.com>
2018-03-26 17:56:47 +0100

    Added lodash

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 const jimp = require('jimp');
-const winston = require('winston');
+const logger = require('./config/logger');
+const config = require('./config/config.json');
+const constants = require('./config/const.json');
+const _ = require('lodash');
 
+// resizer.addImage(image)
+// resizer.addFolder(folder)
+// resizer.options : {}
+// resizer.start()
+// resizer.clear()
+
+exports.addImage = () => {
+    return 'addImage';
+};
+
+exports.addFolder = () => {
+    return 'addFolder';
+};
+
+exports.options = config.defaultOptions;
+
+exports.start = () => {
+    return 'start';
+};
+
+exports.clear = () => {
+    return 'clear';
+};
+
+exports.returnPlatform = () => {
+    return _.get(constants, exports.options.platform);
+};
+
+
+var exports = module.exports = {};
