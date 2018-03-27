8bc46994b9aa542b9d339be90a241a6b54b21bd0
Craig Currie <craig@blitzkraig.com>
2018-03-27 13:55:40 +0100

    Added more resizing functionality

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const jimp = require('jimp');
 const logger = require('./logger');
 const config = require('./config/config.json');
 const constants = require('./config/const.json');
+const fs = require('fs');
 const _ = require('lodash');
 
 exports.options = config.defaultOptions;
 
-// resizer.addImage(image)
-// resizer.addFolder(folder)
-// resizer.options : {}
-// resizer.start()
-// resizer.clear()
-exports.addImage = (imagePath) => {
+exports.addImage = (imagePath) => new Promise((res) => {
     // Verify file here
     exports.options.inputFiles.push(imagePath);
 
-    return logger.info('Added ' + imagePath);
-};
+    return res(logger.info('Added ' + imagePath));
+});
 
-exports.addFolder = () => {
+exports.addFolder = (directoryPath) => new Promise((res) => {
     // Verify folder contains images, loop through and add here
-    return 'addFolder';
-};
+    fs.readdirSync(directoryPath).forEach(file => {
+        exports.addImage(directoryPath + '/' + file);
+    });
+
+    return res(logger.info('Folder added'));
+});
 
 exports.setOutputFolder = (outputPath) => {
     exports.options.outputFolder = outputPath;
@@ -31,44 +31,59 @@ exports.setOutputFolder = (outputPath) => {
     return logger.info('Output folder set: ' + outputPath);
 };
 
-exports.start = () => {
+exports.start = () => new Promise((res, rej) => {
     var ready = true;
-    _.forOwn(exports.options, function(value, key) {
+    _.forOwn(exports.options, (value, key) => {
         if (value.length === 0) {
             ready = false;
-
-            return logger.info('Please define option: ' + key);
+            logger.info('Please define option: ' + key);
         }
     });
 
     if (ready) {
         logger.info('Options valid. Processing...');
 
-        return processFiles();
+        return res(processFiles().then(logger.info('Files processed')));
     } else {
-        return logger.error('Missing options. Aborting.');
+        return rej(logger.error('Missing options. Aborting.'));
     }
-};
+});
 
 exports.clear = () => {
-    return 'clear';
+    exports.options = {};
+
+    return exports.options = config.defaultOptions;
 };
 
 exports.returnPlatform = () => {
-    return _.get(constants, this.options.platform);
+    return _.get(constants, exports.options.platform);
 };
 
-var processFiles = () => {
-    _.forOwn(exports.options.inputFiles, function(value, key) {
+var calculateScale = (input, output, platform) => {
+
+    var inputFactor = _.get(constants.density, platform + '.' + input);
+    var outputFactor = _.get(constants.density, platform + '.' + output);
+
+    return outputFactor / inputFactor;
+};
+
+var processFiles = () => new Promise((res, rej) => {
+    _.forOwn(exports.options.inputFiles, (value, key) => {
         logger.info(key + ' : ' + value);
-        var fileName = value.split('/');
-        fileName = fileName[fileName.length - 1];
-        jimp.read(value).then(function (image) { // TODO Loop per density
-            return image.scale(2) // TODO Calculate desired output here
-                 .quality(60)
-                 .write(exports.options.outputFolder+'/'+fileName); // TODO Ensure slash exists in option
-        }).catch(function (err) {
-            console.error(err);
+        var fileName = value.split('/').pop();
+
+        _.forOwn(exports.options.outputDensity, (density) => { // TODO check PNGs, gifs etc. work
+            jimp.read(value).then(function(image) {
+                return image.scale(calculateScale(exports.options.sourceDensity, density, exports.options.platform))
+                    .quality(60)
+                    .write(exports.options.outputFolder + '/'  + density + '/' + fileName); // TODO Ensure slash exists in option
+            }).catch(function(err) {
+                console.error(err);
+
+                return rej(console.error('Err in processFiles: ' + err));
+            });
         });
     });
-};
+
+    return res('Success');
+});
--- tests/quicktest.js
+++ tests/quicktest.js
@@ -1,6 +1,11 @@
 var resizer = require('../index.js');
 
-resizer.addImage('../testdata/mdpi/testimg.jpeg');
+// resizer.addImage('../testdata/mdpi/testimg.jpeg');
+resizer.addFolder('../testdata/mdpi');
 // resizer.addImage('../testdata/mdpi/testimg2.jpeg');
-resizer.setOutputFolder('../testdata/hdpi');
-resizer.start();
+resizer.setOutputFolder('../testdata');
+resizer.start().then(() => {
+    console.log('Successful run');
+}, () => {
+    console.log('Failed run');
+});
